ad94a68035b45c35a0bdf3746cb745a6e6aae7f9
AS3-1-01 <AS3-1-01@uni.lan>
2019-03-12 13:20:56 +0100

    12/3/2019

--- 3ra_evaluacion/ejemplo-01/ejemplo1.js
+++ 3ra_evaluacion/ejemplo-01/ejemplo1.js
@@ -1,26 +1,22 @@
 /*
 ejemplo 01
 */
-/*
-x=6
-y=5
-w=4
-v=8
-u=7
-*/
 
 
+
+z= -1
+while (z != 10){
 x=Math.floor(Math.random() * 10);
 y=Math.floor(Math.random() * 10);
 w=Math.floor(Math.random() * 10);
 v=Math.floor(Math.random() * 10);
 u=Math.floor(Math.random() * 10);
-
 z=(((x+y)-w)*v)/u
 
-//z= Math.floor(Math.random() * 10);
+//z= Math.floor(Math.random() * 11);
 
 if(z>10){ console.log("es mayor que 10")}
 else if(z<10){ console.log("es menor que 10")}
 else { console.log("es 10")}
  console.log("la z es=" + z)
+}
